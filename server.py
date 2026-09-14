7ab3678e1152805cad4a0a6128d52b52972fd8c0
Roy Folami <roy.folami@gmail.com>
2026-09-14 15:28:18 -0400

    Create server.py flask application with /emotionDetector decorator tested working

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 # Use the following command  to test use the followingh curl command
 #   "curl -X GET -i http://localhost:5000/emotionDetector?textToAnalyze=I%20love%20stuff"
 
-Output URL request response
+"""Output URL request response
 
 """
 from flask import Flask, render_template, request
 
-from EmotionDetection.emotion_detection import emotion_deetector
+from EmotionDetection.emotion_detection import emotion_detector
 
 # Instantiate Flask functionality
 app = Flask("__name__")
@@ -35,15 +35,12 @@ def sent_detector():
         return "No text submitted! Try again."
 
     # Pass text_to_analyzer to sentiment_analyzer then store returned directory in response object.
-    response = sentiment_analyzer(text_to_analyze)
-
-    # Label has value of "None" whch indicates error 500 or non 200repsonse.status_ code returned
-    if label is None:
-        return "Invalid input! Try again."
+    response = emotion_detector(text_to_analyze)
 
     # Return a formatted string with response and dominant_emotion label.
-    result = f"For the given statement, the system response is {response}. The dominant emotion is {response['dominant_emotion']}."
+    result = f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']}, 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
     return result
+
 # If call the server is to "/" the render the GUI tenplate index.htm;.
 @app.route("/")
 def render_index_page():
